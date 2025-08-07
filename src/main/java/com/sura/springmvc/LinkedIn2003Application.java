94cf8c9fc41f7da1c01b158673b36663b0b38944
sujitrahul <sujithrahul27@gmail.com>
2025-08-07 23:09:22 +0530

    Inital Commit

---
+++ src/main/java/com/sura/springmvc/LinkedIn2003Application.java
@@ -0,0 +1,13 @@
+package com.sura.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedIn2003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedIn2003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sura/springmvc/LinkedIn2003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sura.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedIn2003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
