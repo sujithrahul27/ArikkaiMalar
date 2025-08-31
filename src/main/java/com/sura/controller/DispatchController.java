81c69b157984c69d7cec8d0d01983cef752aa16b
sujitrahul <sujithrahul27@gmail.com>
2025-08-31 23:31:57 +0530

    Removed front end as i am not confident but intorduced swagger api and though spring security causing some issues

--- src/main/java/com/sura/controller/DispatchController.java
+++ src/main/java/com/sura/controller/DispatchController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.PutMapping;
@@ -12,27 +11,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sura.model.Job;
-import com.sura.model.Search;
 import com.sura.service.JobsDbService;
 
 @RestController
 public class DispatchController {
 	@Autowired 
 	JobsDbService ds;
-	@Autowired
-	Search s;
+	
 	
 	@GetMapping("/getjobs")
 	public List<Job> getJobs(){
 		return ds.retriveJobs();
 	}
 	
-	@GetMapping("/searchskill/{skill}")
-	public List<Job> searchJobs(@PathVariable("skill") String skill){
-		
-		return s.searchByText(skill);
-		
-	}
+
 	
 	@PostMapping("/postjobs")
 		public List<Job> putJobs(@RequestBody List<Job> jobs){
